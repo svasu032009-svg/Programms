3b55a7e322c47f9d68e2c62d0c3b8cb2cb468a4d
svasu032009-svg <s.vasu032009@gmail.com>
2026-04-25 11:35:15 +0530

    Create sam.java

---
+++ Java Programs/sam.java
@@ -0,0 +1,13 @@
+PROGRAM:
+=========================================
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("");
+    }
+}
+
+========================================
+OUTPUT:
+  I am Vasu
